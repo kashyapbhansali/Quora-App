196272dcae274d81bb284161e899e6dc1a516ebb
TheCodeDragon <kashyapbhansali7@gmail.com>
2014-08-26 21:42:57 +0530

    First upload
    Quora app for windows phone. http://bit.ly/kkbwpapp

---
+++ QuoraApp/MainBrowser.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+using Microsoft.Phone.Net.NetworkInformation;
+
+namespace QuoraApp
+{
+    /*
+    public class UriFormatException : FormatException
+    {
+        public UriFormatException()
+        {
+            
+        }
+    }
+     */
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            Application.Current.Host.Settings.EnableFrameRateCounter = false;
+
+           
+            MiniBrowser.Navigate(new Uri("http://www.quora.com/"));
+            MiniBrowser.IsScriptEnabled=true;
+            MiniBrowser.IsGeolocationEnabled = true;
+
+            MiniBrowser.Navigated += new EventHandler<System.Windows.Navigation.NavigationEventArgs>(MiniBrowser_Navigated);
+            MiniBrowser.Navigating += new EventHandler<NavigatingEventArgs>(MiniBrowser_Navigating);
+            MiniBrowser.ScriptNotify += new EventHandler<NotifyEventArgs>(MiniBrowser_ScriptNotify);
+
+            
+
+            
+        }
+
+
+        
+
+        void MiniBrowser_ScriptNotify(object sender, NotifyEventArgs e)
+        {
+            MiniBrowser.Navigate(new Uri(e.Value, UriKind.Absolute));
+        }
+
+        void MiniBrowser_Navigating(object sender, NavigatingEventArgs e)
+        {
+            ProgBar.Visibility = Visibility.Visible;
+        }
+
+        void MiniBrowser_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            ProgBar.Visibility = Visibility.Collapsed;
+        }
+
+        private void Home_Click(object sender, EventArgs e)
+        {
+           
+           
+            MiniBrowser.Navigate(new Uri("http://www.quora.com/"));
+            //Do work for your application here.
+        }
+
+        private void Follow_Click(object sender, EventArgs e)
+        {
+            MiniBrowser.Navigate(new Uri("http://www.quora.com/Kashyap-Bhansali"));
+            //Do work for your application here.
+        }
+        private void Blog_Click(object sender, EventArgs e)
+        {
+            MiniBrowser.Navigate(new Uri("http://techbum.blogspot.com/"));
+            //Do work for your application here.
+        }
+
+        private void OpenQuestions_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("Button: Back works!");
+            MiniBrowser.Navigate(new Uri("http://www.quora.com/open_questions"));
+            //Do work for your application here.
+        }
+
+        private void Credits_Click(object sender, EventArgs e)
+        {
+            MiniBrowser.Navigate(new Uri("http://www.quora.com/credits"));
+            //Do work for your application here.
+        }
+
+        private void Content_Click(object sender, EventArgs e)
+        {
+            MiniBrowser.Navigate(new Uri("http://www.quora.com/content"));
+            //Do work for your application here.
+        }
+
+        private void Forward_Click(object sender, EventArgs e)
+        {
+           // MessageBox.Show("Button: forward works!");
+            MiniBrowser.GoForward();
+            //Do work for your application here.
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+           // MessageBox.Show("Button: Back works!");
+            MiniBrowser.GoBack();
+            //Do work for your application here.
+        }
+
+        private void Notifications_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("Button: Back works!");
+            MiniBrowser.Navigate(new Uri("http://www.quora.com/notifications"));
+            //Do work for your application here.
+        }
+
+        private void Stats_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("Button: Back works!");
+            MiniBrowser.Navigate(new Uri("http://www.quora.com/stats"));
+            //Do work for your application here.
+        }
+        
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+
+        }
+
+
+        
+    }
+
+    
+}
+
---
+++ QuoraApp/MainPage.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using QuoraApp.Resources;
+using Microsoft.Phone.Tasks;
+using Microsoft.Phone.Net.NetworkInformation;
+using System.Threading;
+
+namespace QuoraApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            var ni = NetworkInterface.NetworkInterfaceType;
+
+            //Checking for internet connection...
+            if ((ni == NetworkInterfaceType.Wireless80211) || (ni == NetworkInterfaceType.MobileBroadbandCdma) || (ni == NetworkInterfaceType.MobileBroadbandGsm))
+            { }
+
+            else if (ni == NetworkInterfaceType.None)
+            {
+                MessageBox.Show("Not connected to Internet. Please check your connection!");
+            }
+
+            InitializeComponent();
+            
+            Application.Current.Host.Settings.EnableFrameRateCounter = false;
+            
+            /*
+            if (!checkNetworkConnection())
+            {
+                MessageBox.Show("Not connected to Internet. Please check your connection!");
+            }
+            */
+            
+
+
+        }
+        /*
+        public static bool checkNetworkConnection()
+        {
+            var ni = NetworkInterface.NetworkInterfaceType;
+
+            bool IsConnected = false;
+            if ((ni == NetworkInterfaceType.Wireless80211) || (ni == NetworkInterfaceType.MobileBroadbandCdma) || (ni == NetworkInterfaceType.MobileBroadbandGsm))
+                IsConnected = true;
+            else if (ni == NetworkInterfaceType.None)
+                IsConnected = false;
+            return IsConnected;
+        }*/
+
+       
+
+
+        private void passParam_Click(object sender, RoutedEventArgs e)
+        {
+
+            NavigationService.Navigate(new Uri("/MainBrowser.xaml"));
+        }
+
+        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rate_Click(object sender, RoutedEventArgs e)
+        {
+            var a = new MarketplaceReviewTask();
+            a.Show();
+           
+            //MessageBox.Show("Thanks for rating this app. We are working to make it better!");
+        }
+
+        private void Share_Click(object sender, RoutedEventArgs e)
+        {
+            ShareLinkTask shareLinkTask = new ShareLinkTask();
+
+            shareLinkTask.Title = "Quora Client for Windows Phone";
+            shareLinkTask.LinkUri = new Uri("http://bit.ly/kkbwpapp", UriKind.Absolute);
+            shareLinkTask.Message = "New Quora Client for Windows Phone! Well designed UI";
+
+            shareLinkTask.Show();
+        }
+
+        private void Mail_Click(object sender, RoutedEventArgs e)
+        {
+           // EmailComposeTask emailComposeTask = new EmailComposeTask();
+
+            //emailComposeTask.Subject = "Check out the new Quora Client for WP!";
+            //emailComposeTask.Body = "Quora Client is a new app for windows phone 7.x/8.x. I feel should try it out too! :)";
+            //emailComposeTask.Show();
+
+            //MessageBox.Show("Thanks for rating this app. We are working to make it better!");
+            ShareLinkTask shareLinkTask = new ShareLinkTask();
+
+            shareLinkTask.Title = "Quora Client";
+            shareLinkTask.LinkUri = new Uri("http://bit.ly/kkbwpapp", UriKind.Absolute);
+            shareLinkTask.Message = "Here great Quora Client for Windows Phone. Try it out!";
+
+            shareLinkTask.Show();
+        }
+
+        private void Button1_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainBrowser.xaml", UriKind.Relative));
+        }
+
+       
+
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
